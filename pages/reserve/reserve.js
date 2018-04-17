a4fd78f621665c430bce7fe2d4498a10e5d2496d
杨超 <yangchao@babyfs.cn>
2018-04-17 17:20:41 +0800

    add schedule table

--- pages/reserve/reserve.js
+++ pages/reserve/reserve.js
@@ -1,6 +1,6 @@
 // pages/reserve/reserve.js
+var that
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -11,103 +11,114 @@ Page({
       "10:40",
       "11:00",
       "11:20",
-      "11:40"
+      "11:40",
+      "12:00",
+      "14:00",
+      "14:20",
+      "14:40"
     ],
-    arrange:[
-      [
-        {
-          cd: "小明",
-          ft: "Brint",
-          time: "10:00",
-          event: "音频",
-          studio: "F16"
-        },
-        {
-          cd: "小红",
-          ft: "Brint",
-          time: "10:20",
-          event: "视频频",
-          studio: "F25"
-        }
-      ],
-      [
-        {
-          cd: "小明",
-          ft: "Audrey",
-          time: "10:00",
-          event: "音频",
-          studio: "F16"
-        },
-        {
-          cd: "小红",
-          ft: "Audrey",
-          time: "10:20",
-          event: "视频频",
-          studio: "F25"
-        }
-      ]
-    ]
+    arrange: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    that = this
+    console.log(options.query)
+    requestArrange("20180909");
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
-  openSchedule: function () {
+  openSchedule: function (event) {
     wx.navigateTo({
-      url: '../schedule/schedule' 
+      url: '../schedule/schedule?schedule=' + JSON.stringify(event.currentTarget.dataset.schedule)
     });
   }
 })
+
+function requestArrange(date) {
+  wx.request({
+    url: 'http://localhost:8000/cdmanager/arrange/' + date,
+    data: {
+      // x: '',
+      // y: ''
+    },
+    header: {
+      'content-type': 'application/json'
+    },
+    success: function (res) {
+      console.log(res.data)
+      for (var j = 0; j < res.data.length; j++) {
+        var scheduleList = [];
+        for (var i = 0; i < that.data.timeList.length; i++) {
+          var schedule = {};
+          schedule["timeIndex"] = -1;
+          scheduleList.push(schedule)
+        }
+
+        var scheduleByFT = res.data[j];
+        for (var i = 0; i < scheduleByFT.schedule.length; i++) {
+          if (scheduleByFT.schedule[i].timeIndex < scheduleList.length) {
+            scheduleList[scheduleByFT.schedule[i].timeIndex] = scheduleByFT.schedule[i];
+          }
+        }
+        res.data[j].schedule = scheduleList
+
+        console.log(res.data)
+      }
+
+      that.setData({
+        arrange: res.data
+      })
+    }
+  })
+}
--- pages/schedule/schedule.js
+++ pages/schedule/schedule.js
@@ -5,62 +5,95 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    eventIndex: 0,
+    studioIndex: 0,
+    schedule: {},
+    eventArray: ['视频', '音频'],
+    studioArray: ['F16', 'F25']
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log(options)
+    this.setData({
+      schedule: JSON.parse(options.schedule)
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  }
+
+  },
+
+  cancelSchedule: function() {
+    console.log("close schedule")
+    wx.navigateBack({
+      url: "../reserve/reserve"
+    })
+  },
+
+  confirmSchedule: function() {
+    // 提交预约
+
+  },
+
+  bindEventChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      eventIndex: e.detail.value
+    })
+  },
+
+  bindStudioChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      studioIndex: e.detail.value
+    })
+  },
 })
